ae04eb2a24db1f0bdc387f5b4c715ce3ec1cb463
atlno1fan <72287889+atlno1fan@users.noreply.github.com>
2023-08-28 08:45:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', type=int, default='bert')
+    parser.add_argument('--model', type=str, default='bert')
     args = parser.parse_args()
     if args.model == 'bert':
         run_experiment_bert4rec()
